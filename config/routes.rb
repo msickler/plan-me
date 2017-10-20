324a7c00cb8d88a24b594cde41bcf33f0d529818
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-20 23:54:34 +0000

    added contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'home/contact'
   resources :goers
   resources :planners
   resources :trips
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
