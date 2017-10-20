f6605242cdee9dbcdec479f30d6f05f28ab3b4ee
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-20 15:53:34 +0000

    adding edit and update for planners

--- app/controllers/planners_controller.rb
+++ app/controllers/planners_controller.rb
@@ -10,13 +10,23 @@ class PlannersController < ApplicationController
       session[:planner_id] = @planner.id
       redirect_to planner_path(@planner)
     else
-      redirect_to "/login"
+      redirect_to new_planner_path
     end
+  end
+
+  def edit
+    @planner = Planner.find(params[:id])
   end
 
   def show
     @planner = Planner.find_by(id: params[:id])
   end
+
+  def update
+		@planner = Planner.find(params[:id])
+		@planner.update(email: params[:email], personality: params[:personality], reason: params[:reason], international: params[:international])
+		redirect_to planner_path(@planner)
+	end
 
   private
     def planner_params
