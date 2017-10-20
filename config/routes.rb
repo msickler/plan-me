08e48bc8a9f7bc7efb8edb10e767b086d72bb807
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-20 16:52:58 +0000

    adding theme for trip posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :goers
   resources :planners
+  resources :trips
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
