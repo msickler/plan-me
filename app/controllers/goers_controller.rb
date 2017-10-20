ca3bb8f35a6ed8a33a77eaf0c9d5faa39366034b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-20 16:12:26 +0000

    added show and linkto edit for planner

--- app/controllers/goers_controller.rb
+++ app/controllers/goers_controller.rb
@@ -8,9 +8,8 @@ class GoersController < ApplicationController
     @goer = Goer.new(goer_params)
     if @goer.save
       session[:goer_id] = @goer.id
-      redirect_to 'home/index'
+      redirect_to goer_path(@goer)
     else
-
       redirect_to new_goer_path
     end
   end
@@ -18,6 +17,12 @@ class GoersController < ApplicationController
   def show
     @goer = Goer.find_by(id: params[:id])
   end
+
+  def update
+		@goer = Goer.find(params[:id])
+		@goer.update(email: params[:email], personality: params[:personality], reason: params[:reason], budget: params[:budget], companion: params[:companion], international: params[:international])
+		redirect_to goer_path(@goer)
+	end
 
   private
     def goer_params
