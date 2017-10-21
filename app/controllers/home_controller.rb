381825b1a919927b0588ce91b45f3c98e6dcd6f2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-21 19:53:45 +0000

    finding issues might have to resturcture user models inheritance

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,11 @@ class HomeController < ApplicationController
   def index
   end
 
+  def main
+    unless logged_in?
+      redirect_to '/'
+    end
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/contact'
+  get 'home/main'
   delete 'logout' => 'sessions#destroy'
   resources :goers
   resources :planners
