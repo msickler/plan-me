80dc3f5176ed3ba9c5850f8326f5f8c4a68bd64d
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-23 14:49:30 +0000

    adjusting omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,4 @@
 class SessionsController < ApplicationController
-  #def test
-  #  @user = User.find_or_create_by(uid: auth['uid']) do |u|
-  #    u.name = auth['info']['name']
-  #    u.email = auth['info']['email']
-  #    u.image = auth['info']['image']
-  #  end
-  #  session[:user_id] = @user.id
-  #  render '/'
-  #end
 
   def new
   end
@@ -18,23 +9,18 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash = request.env['omniauth.auth']
-      oauth_email = request.env['omniauth.auth']['email']
-      if user = User.find_by(email: oauth_email)
-        session[:user_id] = user.id
-      else
-        user = User.create(email: oauth_email)
-        session[:user_id] = user.id
-      end
-
+      @user = User.find_or_create_by_omniauth(auth_hash)
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       @user = User.find_by(email: params[:user][:email])
-      if @user && @user.authenticate(params[:session][:password])
+      if @user && @user.authenticate(params[:user][:password])
         flash[:notice] = "#{@user.name} successfully logged in."
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
         flash[:notice] = "Something went wrong. Please try again."
-        redirect_to "/sessions/new"
+        redirect_to "sessions/new"
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :international, inclusion: { in: [true, false] }
 
-end 
+  def self.find_or_create_by_omniauth(auth_hash)
+    where(email: auth_hash['info']['email']).first_or_create do |user|
+      user.password = SecureRandom.hex
+    end
+  end
+
+end
