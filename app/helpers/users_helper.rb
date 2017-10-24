694044f9eb1fa109b7e530777ab972400de60137
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-24 14:07:38 +0000

    trying to fix trip form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,5 @@
 module UsersHelper
 
-  def international?(user)
-    if user.international == true
-      "Global"
-    else
-      user.international == false
-      "The US"
-    end
-  end
 
 
 end
